8115fb1e0081cfa996373c909400fefe576c0b91
riyanbagasdp <riyanbagasdp@student.uns.ac.id>
2022-08-29 22:41:45 +0700

    upload tugas PBO

---
+++ MyProject2/src/myproject2/PBO1.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myproject2;
+
+/**
+ *
+ * @author lenovo
+ */
+public class PBO1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("RRRRRR");
+        System.out.print("    ");
+        System.out.print("II");
+        System.out.print("  ");
+        System.out.print("YY");
+        System.out.print("    ");
+        System.out.println("YY");
+        System.out.print("RR");       
+        System.out.print("   "); 
+        System.out.print("RR"); 
+        System.out.print("   ");
+        System.out.print("II");
+        System.out.print("   ");
+        System.out.print("YY");
+        System.out.print("  ");
+        System.out.println("YY");
+        System.out.print("RRRRRR"); 
+        System.out.print("    ");
+        System.out.print("II");
+        System.out.print("    ");
+        System.out.println("YYYY");
+        System.out.print("RRRRR");
+        System.out.print("     ");
+        System.out.print("II"); 
+        System.out.print("     ");
+        System.out.println("YY");
+        System.out.print("RR");
+        System.out.print("  "); 
+        System.out.print("RR");       
+        System.out.print("    ");
+        System.out.print("II");
+        System.out.print("    ");
+        System.out.println("YY");
+        System.out.print("RR");
+        System.out.print("   ");
+        System.out.print("RR");       
+        System.out.print("   ");
+        System.out.print("II");
+        System.out.print("   ");
+        System.out.println("YY");
+
+    }
+    
+}
